db452b5c73028cbf699854b650941ba4a357837d
schwann2402 <139656534+schwann2402@users.noreply.github.com>
2024-08-29 13:00:15 -0500

    added github-action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: "https://schwann2402.github.io",
+  base: "personal_portfolio",
 });
